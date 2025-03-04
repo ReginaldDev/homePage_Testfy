7e911a51a884a404942cc6034674473fffc14a02
Reginaldo Junior <rsajr12@gmail.com>
2025-03-04 09:43:07 -0300

    atualizando css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Adiciona comportamento ao menu sanduíche
     const navbarToggler = document.querySelector(".navbar-toggler");
     const navbarMenu = document.querySelector("#navbarNav");
+    const bsCollapse = new bootstrap.Collapse(navbarMenu, { toggle: false });
 
+    // Adiciona comportamento ao menu sanduíche
     navbarToggler.addEventListener("click", function () {
-        navbarMenu.classList.toggle("show");
+        if (navbarMenu.classList.contains("show")) {
+            bsCollapse.hide(); // Fecha o menu
+        } else {
+            bsCollapse.show(); // Abre o menu
+        }
     });
 
     // Fechar o menu quando qualquer link for clicado e aplicar scroll suave
     document.querySelectorAll(".nav-link").forEach(anchor => {
         anchor.addEventListener("click", function (event) {
-            // Fecha o menu
-            navbarMenu.classList.remove("show");
-            
-            // Scroll suave para seções
             event.preventDefault();
+
+            // Fecha o menu ao clicar em um link
+            bsCollapse.hide();
+
+            // Scroll suave para a seção correspondente
             const targetId = this.getAttribute("href").substring(1);
             document.getElementById(targetId).scrollIntoView({
                 behavior: "smooth"
             });
         });
     });
-
 });
+
 
 
